11b506e86e664aa1ae233c42a98e2b2391eacbce
Jonathan Gottfried <jonmarkgo@gmail.com>
2013-05-04 17:21:47 -0400

    Set up scope, approval_prompt, and access_type

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"],
+           {
+             :scope => "https://www.googleapis.com/auth/glass.timeline,https://www.googleapis.com/auth/userinfo.profile",
+             :approval_prompt => "auto",
+             :access_type => "offline
+           }
 end
