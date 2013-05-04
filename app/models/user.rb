cf21ad853eccba51f78f7ec45d591849576e2eef
Jonathan Gottfried <jonmarkgo@gmail.com>
2013-05-04 17:33:41 -0400

    Add omniauth controller and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+        user = User.create(name: data["name"],
+	    		   email: data["email"],
+	    		   password: Devise.friendly_token[0,20]
+	    		  )
+    end
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GlassRails::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
