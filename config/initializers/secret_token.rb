6a0bad2473987ebd05e1d661e2a36aadfd5985a5
Jonathan Gottfried <jonmarkgo@gmail.com>
2013-05-04 17:15:08 -0400

    New rails 3.2.13 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassRails::Application.config.secret_token = '93cb2bde2ed656399e32810e9146eceecce176f5834b607d1f2bcfc261552eab89e6c778257b6b187bee281a9833b8a1c54be1b7b5aa43b8e064e00fb3486b10'
