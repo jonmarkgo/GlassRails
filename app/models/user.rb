92971398b86f61c5010b94b802c1c7293227dd7e
Jonathan Gottfried <jonmarkgo@gmail.com>
2013-05-04 17:55:06 -0400

    Don't use name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
@@ -14,8 +14,7 @@ class User < ActiveRecord::Base
     user = User.where(:email => data["email"]).first
 
     unless user
-        user = User.create(name: data["name"],
-	    		   email: data["email"],
+        user = User.create(email: data["email"],
 	    		   password: Devise.friendly_token[0,20]
 	    		  )
     end
