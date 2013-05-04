18bd5e1f9a1b21076cd44d50618caab256a1b486
Jonathan Gottfried <jonmarkgo@gmail.com>
2013-05-04 19:09:05 -0400

    Properly save tokens upon authentiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,24 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+
     data = access_token.info
+
+     #   puts data.inspect
     user = User.where(:email => data["email"]).first
 
     unless user
-        user = User.create(email: data["email"],
-	    		   password: Devise.friendly_token[0,20],
-             access_token: data["access_token"],
-             refresh_token: data["refresh_token"],
-             token_expires_at: Time.at(data["expires_at"])
-	    		  )
+        user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+            )
+        user.access_token = access_token.credentials.token
+        user.refresh_token = access_token.credentials.refresh_token
+        user.token_expires_at = Time.at(access_token.credentials.expires_at)
     end
     user
   end
